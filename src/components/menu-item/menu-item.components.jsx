403f586b8d4d474c431b7a6b155f324d7261b74a
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-06 15:38:40 +0800

    add routing to the app and hats component

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div style={{
             backgroundImage: `url(${imageUrl})`
         }} className='background-image'/>
-        <div className='content'>
+        <div className='content'> 
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>Shop Now</span>
         </div>
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
