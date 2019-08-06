26a4e2e4a2b053700218d1d44c9d6636bd05199c
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-06 21:24:32 +0800

    add collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((items, idx) => idx < 4).map(item => (
-                <div key={item.id}>{item.name}</div>
+                items.filter((items, idx) => idx < 4).map(({id, ...itemProps}) => (
+                <CollectionItem key={id} {...itemProps}/>
             ))
             }
         </div>
