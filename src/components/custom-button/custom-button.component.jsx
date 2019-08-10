990fdefb92474d22d89354d55b930613a2ed63be
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-10 22:36:09 +0800

    add sign-out feature

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, ...customButtonProps}) => (
-    <button className='custom-button' {...customButtonProps}>{children}</button>
+const CustomButton = ({children, isGoogleSignIn, ...customButtonProps}) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...customButtonProps}>{children}</button>
 )
 
 export default CustomButton;
