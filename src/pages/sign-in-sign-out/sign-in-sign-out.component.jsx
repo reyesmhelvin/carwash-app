cff0f02b9ed216b15c25ff3dbc47ca9d6e0e0891
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-10 17:13:57 +0800

    add sign-in-sign-out page

---
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sign-in-sign-out.styles.scss';
+
+const SignInSignOut = () => (
+    <div className='sign-in-sign-out'>SIGN IN</div>   
+)
+
+export default SignInSignOut;
