a378d852c8bd78487cc7d7dab8269e4c252df8cd
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-11 19:48:35 +0800

    add styling for add to cart

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...customButtonProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...customButtonProps}>{children}</button>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...customButtonProps}) => (
+    <button className={`
+    ${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''} 
+    custom-button`
+} {...customButtonProps}>{children}</button>
 )
 
 export default CustomButton;
