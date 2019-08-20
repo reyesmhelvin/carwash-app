b66b2a2e3489d8aa956e18f43fd8517120ea50eb
Reyes, Mhelvin O <mhelvin.o.reyes@accenture.com>
2019-08-20 21:00:57 +0800

    add increase and decrease functionality in the checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,18 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
     return [...cartItems, {...cartItemsToAdd, quantity: 1}];
 
 }
+
+export const removeItemToCart = (cartItems, cartItemToRemove) => {
+    const existingItem = cartItems.find(cartitem => cartitem.id === cartItemToRemove.id);
+    console.log(existingItem);
+
+    if (existingItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id 
+        ? { ...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    );
+}
